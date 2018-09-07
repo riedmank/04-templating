474b4c49849cfd48cb26b087887dc1de1c87a533
kris riedman <riedmank@gmail.com>
2018-09-07 09:50:42 -0700

    add normalize file

--- kris-andrew/scripts/article.js
+++ kris-andrew/scripts/article.js
@@ -11,8 +11,12 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
+const source = $('article-template').html();
+const template = Handlebars.compile(source);
+
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
@@ -31,7 +35,7 @@ Article.prototype.toHtml = function() {
   // }
 
   // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+  return template(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
